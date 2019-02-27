ecc4b0e39daa243a7fa3a26a6528d6acb4061c00
Vince Seely <vince@vinceseely.com>
2019-02-27 17:06:17 -0600

    craeted a gear and gear changer as well as started working on the ui

---
+++ Gear.py
@@ -0,0 +1,24 @@
+import random
+
+
+class Gear():
+    def __init__(self, max_pos, total_gears):
+        self.min_turn = 0
+        self.max_turn = 4
+        if max_pos > self.max_turn:
+            self.max_turn = 1
+        self.position = random.randint(0, max_pos)
+        self.max_position = max_pos
+        self.rotations = self.random_gears(total_gears)
+
+    def random_gears(self, total_gears):
+        other_gear_rotations = []
+        for x in range(total_gears):
+            other_gear_rotations.append(random.randint(self.min_turn, self.max_turn))
+        return other_gear_rotations
+
+    def turn(self, times):
+        self.position = (self.position + times) % self.max_position
+
+    def get_position(self):
+        return self.position + 1
---
+++ GearChanger.py
@@ -0,0 +1,27 @@
+import random
+import Gear
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.button import Label
+
+
+class GearMainApp:
+    def __init__(self):
+        self.gears = []
+        self.generate_gears(8)
+        self.gearChanger = GearChanger()
+
+    def generate_gears(self, number_of_gears):
+        for x in range(number_of_gears):
+            self.gears.append(Gear(random.randint(0, 100), number_of_gears))
+        print(self.gears)
+
+
+class GearChanger(App):
+    def build(self):
+        return Label()
+
+
+gearApp = GearChanger()
+gearApp.run()
