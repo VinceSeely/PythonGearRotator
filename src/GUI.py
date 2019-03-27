f8c6314a5c38f45e003665a8a27608c0a8aaed27
Vince Seely <vince@vinceseely.com>
2019-03-27 12:19:14 -0500

    display user friendly numbers

--- src/GUI.py
+++ src/GUI.py
@@ -34,14 +34,14 @@ class GUI:
         self.toolbar.pack(side=TOP, anchor=W, padx=10)
 
         self.gearValues = Listbox(self.root, width=20, height=19)
-        self.gearValues.pack(side=LEFT, padx=(10,0))
-        
+        self.gearValues.pack(side=LEFT, padx=(10, 0))
+
         self.scrollbar = Scrollbar(orient="vertical")
         self.scrollbar.config(command=self.gearValues.yview)
         self.scrollbar.pack(side="left", fill="y", pady=10)
-        self.bottomScroll = Scrollbar(orient="horizontal")
-        self.bottomScroll.config(command=self.gearValues.yview)
-        self.bottomScroll.pack(side="left")
+        # self.bottomScroll = Scrollbar(orient="horizontal")
+        # self.bottomScroll.config(command=self.gearValues.yview)
+        # self.bottomScroll.pack(side="left")
 
         self.gearValues.config(yscrollcommand=self.scrollbar.set)
 
@@ -58,7 +58,7 @@ class GUI:
         self.goalState.pack(side=TOP)
 
         self.turnsLabel = Label(self.infoPanel, text="Turn order:", width=20, anchor=W)
-        self.turnsList = Label(self.infoPanel, text="", width=20, anchor = W)
+        self.turnsList = Label(self.infoPanel, text="", width=20, anchor=W)
         self.turnsLabel.pack(side=TOP)
         self.turnsList.pack(side=TOP)
 
@@ -90,26 +90,26 @@ class GUI:
         gear1.position = 4
         gear2.position = 2
         gear3.position = 1
-        gear1.rotations = [2,6,5]
-        gear2.rotations = [1,5,1]
-        gear3.rotations = [3,1,4]
-        self.Gears = [gear1,gear2,gear3]
+        gear1.rotations = [2, 6, 5]
+        gear2.rotations = [1, 5, 1]
+        gear3.rotations = [3, 1, 4]
+        self.Gears = [gear1, gear2, gear3]
         positions = ""
         for gear in self.Gears:
-            positions += '{}, '.format(gear.position)
+            positions += '{}, '.format(gear.get_position())
         self.goal = []
         for gear in self.Gears:
             self.goal.append(gear.goal)
         self.gearValues.delete(0, END)
         self.startState["text"] = positions[0:(len(positions) - 2)]
-        self.goalState["text"] = ", ".join(map(str, self.goal))
+        self.goalState["text"] = ", ".join(map(str, [x + 1 for x in self.goal]))
 
     def initBindings(self):
         self.idfsButton.bind("<Button-1>", self.idfs_Handler)
         self.hillClimbButton.bind("<Button-1>", self.hillClimbing_Handler)
         self.aStarButton.bind("<Button-1>", self.aStar_Handler)
         self.generateButton.bind("<Button-1>", self.GenerateGears)
-    
+
     def GenerateGears(self, button):
         self.Gears = []
         numGears = int(self.numGearsEntry.get())
@@ -124,12 +124,12 @@ class GUI:
         self.gearValues.delete(0, END)
         self.startState["text"] = self.getPositionsString(self.gears)
         self.goalState["text"] = goalString
-    
+
     def idfs_Handler(self, button):
         idfs = LimitedDepthFirstSearch()
         self.runAny(idfs)
-    
-    def runAny(self, methodToBeRun):        
+
+    def runAny(self, methodToBeRun):
         t0 = time.time()
         result = methodToBeRun.Run(self.Gears, self.goal)
         t1 = time.time()
@@ -137,8 +137,8 @@ class GUI:
             self.turnsList['text'] = "None"
         else:
             self.printResults(result)
-            self.turnsList['text'] = ", ".join(map(str, [x+1 for x in result]))
-        self.timeText['text'] = t1-t0
+            self.turnsList['text'] = ", ".join(map(str, [x + 1 for x in result]))
+        self.timeText['text'] = t1 - t0
 
     def printResults(self, results):
         self.gearValues.delete(0, END)
@@ -154,7 +154,7 @@ class GUI:
         return gearPositions[0:(len(gearPositions) - 2)]
 
     def hillClimbing_Handler(self, button):
-        self.timeText['text'] = "fuck1"        
+        self.timeText['text'] = "fuck1"
         hillClimbing = HillClimbingSearch(self.goal)
         self.runAny(hillClimbing)
 
