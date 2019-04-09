4e9201d118e9c02987aa6f55e12a17126f1ed01d
Vince Seely <vince@vinceseely.com>
2019-04-09 15:19:21 -0500

    mad limited search no longer recursive

--- src/LimitedDepthFirstSearch.py
+++ src/LimitedDepthFirstSearch.py
@@ -4,13 +4,11 @@ import Gear
 
 
 class LimitedDepthFirstSearch:
-    def __init__ (self):
+    def __init__(self):
         self.closed = []
 
     def Run(self, gear_manager):
         result = self.IterativeSearch(gear_manager, 6, 500)
-        if result is not None:
-            result.reverse()
         return result
 
     def IterativeSearch(self, gear_manager, startLevel, MaxLevel):
@@ -22,31 +20,32 @@ class LimitedDepthFirstSearch:
                 break
             self.closed = []
         self.closed.sort()
-        return result
+        return result[0]
 
     def _LimitedSearch(self, gear_manager, currentLevel, maxLevel, gears):
-        
-        if currentLevel > maxLevel:
-            return None
+        nodes_to_visit = [[[], gears]]
+        while len(nodes_to_visit) != 0:
+            currentNode = nodes_to_visit.pop()
+            if len(currentNode[0]) > maxLevel:
+                continue
 
-        gearPositions = []    
-        for gear in gears:
-            gearPositions.append(gear.position)
+            gearPositions = []    
+            for gear in currentNode[1]:
+                gearPositions.append(gear.position)
 
-        if self.closed.__contains__(gearPositions):
-            return None
+            if self.closed.__contains__(gearPositions):
+                continue
 
-        self.closed.append(gearPositions)
+            self.closed.append(gearPositions)
 
-        if self.closed.__contains__(gear_manager.get_goal()):
-            return []
+            if self.closed.__contains__(gear_manager.get_goal()):
+                return currentNode
 
-        for gearToBeRotated in range(len(gears)):
-            gearCopy = gear_manager.rotate_and_copy(gears, gearToBeRotated)
-            result = self._LimitedSearch(gear_manager, currentLevel + 1, maxLevel, gearCopy)
-            if result is not None:
-                result.append(gearToBeRotated)
-                return result
-            
+            for gearToBeRotated in range(len(gears)):
+                gearCopy = gear_manager.rotate_and_copy(currentNode[1], gearToBeRotated)
+                nodeCopy = copy.deepcopy(currentNode[0])
+                nodeCopy.append(gearToBeRotated)
+                nodes_to_visit.append([nodeCopy, gearCopy])
+
         return None 
 
